f9506834e6a3c1c08e1c7438a77f7fd03bb26e7f
Krzysztof Wrona <krzychuwr1@gmail.com>
2018-04-23 21:13:09 +0200

    Initial commit. A very simple dashboard frontend in blazor

---
+++ BlazorApp/BlazorDashboard/BlazorDashboard.Shared/PricesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorDashboard.Shared
+{
+    public class PricesModel
+    {
+        public decimal USD { get; set; }
+        public decimal EUR { get; set; }
+        public decimal JPY { get; set; }
+    }
+}
