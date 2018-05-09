5dcfced1275d51e635a0363a10547de5dd8495fa
Wookie <lukasz.lampart.priv@gmail.com>
2018-05-09 18:15:10 +0200

    Added missing testcase for interpolation

--- server/src/test/scala/pl/edu/agh/crypto/dashboard/persistence/QueryInterpolationTest.scala
+++ server/src/test/scala/pl/edu/agh/crypto/dashboard/persistence/QueryInterpolationTest.scala
@@ -2,7 +2,8 @@ package pl.edu.agh.crypto.dashboard.persistence
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class QueryInterpolationTest extends FlatSpec with Matchers with QueryInterpolation with QueryParameter.Syntax {
+class QueryInterpolationTest extends FlatSpec with Matchers
+  with QueryInterpolation with QueryParameter.Syntax with SerializationUtils {
 
   "Interpolator" should "work as regular interpolator" in {
     val x = "a"
@@ -19,17 +20,27 @@ class QueryInterpolationTest extends FlatSpec with Matchers with QueryInterpolat
     query.parameters("var_2") should equal(123)
   }
 
+  it should "correctly serialize json object (java map, string to any)" in {
+    case class MyClass(x: Int, y: Int)
+    import io.circe.generic.auto._
+    import java.util.{Map => JMap}
+    val query = aql"${bind(MyClass(3, 4))}"
+
+    query.parameters("var_1") should matchPattern {
+      case m: JMap[_, _] if m.get("x") == 3 && m.get("y") == 4 =>
+    }
+  }
+
   it should "remove duplicates" in {
     case class MyClass(x: Int, y: Int)
     import io.circe.generic.auto._
-    val query =
-      aql"""UPSERT { '_key': ${bindKey("lama") }
-        |INSERT ${bind(MyClass(3, 4))}
-        |UPDATE ${bind(MyClass(3, 4))} IN ${bindCollection("my-collection")}
+
+    val query = aql"""UPSERT { '_key': ${bindKey("lama") }
+         |INSERT ${bind(MyClass(3, 4))}
+         |UPDATE ${bind(MyClass(3, 4))} IN ${bindCollection("my-collection")}
       """.stripMargin
 
-    println(query.parameters)
-    true shouldBe true
+    query.parameters.size should equal(3)
   }
 
 }
