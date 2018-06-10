12ac303b3493cd09fd5a13914edfce701d7b27ee
Krzysztof Wrona <krzychuwr1@gmail.com>
2018-06-11 01:54:28 +0200

    Improved prices graph

--- BlazorApp/BlazorDashboard/BlazorDashboard.Client/wwwroot/scripts/BlazorRegisterFunctions.js
+++ BlazorApp/BlazorDashboard/BlazorDashboard.Client/wwwroot/scripts/BlazorRegisterFunctions.js
@@ -1,40 +1,28 @@
-﻿Blazor.registerFunction('blazor_chartistTest', (low, high) => {
-    // Our labels and three data series
+﻿Blazor.registerFunction('blazor_chartist', (id, labels, series1, series2) => {
     var data = {
-        labels: ['Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6', 'Week7'],
+        labels: labels,
         series: [
-            low,
-            high
+            series1,
+            series2
         ]
     };
 
-    // We are setting a few options for our chart and override the defaults
     var options = {
-        // Don't draw the line chart points
         showPoint: true,
         // Disable line smoothing
         lineSmooth: false,
-        // X-Axis specific configuration
         axisX: {
-            // We can disable the grid for this axis
             showGrid: true,
-            // and also don't show the label
             showLabel: true
         },
-        // Y-Axis specific configuration
         axisY: {
-            // Lets offset the chart a bit from the labels
             offset: 60,
-            // The label interpolation function enables you to modify the values
-            // used for the labels on each axis. Here we are converting the
-            // values into million pound.
             labelInterpolationFnc: function (value) {
                 return '$' + value;
             }
         }
     };
 
-    // All you need to do is pass your configuration as third parameter to the chart function
     new Chartist.Line('.ct-chart', data, options);
     return true;
 });
