f6c5bccd426d9d3a9b39dfcd91bbf1f129b32faf
Krzysztof Wrona <krzychuwr1@gmail.com>
2018-06-10 20:12:26 +0200

    more advanced chartist demo

--- BlazorApp/BlazorDashboard/BlazorDashboard.Client/wwwroot/scripts/BlazorRegisterFunctions.js
+++ BlazorApp/BlazorDashboard/BlazorDashboard.Client/wwwroot/scripts/BlazorRegisterFunctions.js
@@ -1,17 +1,42 @@
 ﻿Blazor.registerFunction('blazor_chartistTest', () => {
+    // Our labels and three data series
     var data = {
-        // A labels array that can contain any sort of values
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-        // Our series array that contains series objects or in this case series data arrays
+        labels: ['Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6'],
         series: [
-            [5, 2, 4, 2, 0]
+            [5, 4, 3, 7, 5, 10],
+            [3, 2, 9, 5, 4, 6],
+            [2, 1, -3, -4, -2, 0]
         ]
     };
 
-    // Create a new line chart object where as first parameter we pass in a selector
-    // that is resolving to our chart container element. The Second parameter
-    // is the actual data object.
-    new Chartist.Line('.ct-chart', data);
+    // We are setting a few options for our chart and override the defaults
+    var options = {
+        // Don't draw the line chart points
+        showPoint: false,
+        // Disable line smoothing
+        lineSmooth: false,
+        // X-Axis specific configuration
+        axisX: {
+            // We can disable the grid for this axis
+            showGrid: false,
+            // and also don't show the label
+            showLabel: false
+        },
+        // Y-Axis specific configuration
+        axisY: {
+            // Lets offset the chart a bit from the labels
+            offset: 60,
+            // The label interpolation function enables you to modify the values
+            // used for the labels on each axis. Here we are converting the
+            // values into million pound.
+            labelInterpolationFnc: function (value) {
+                return '$' + value + 'm';
+            }
+        }
+    };
+
+    // All you need to do is pass your configuration as third parameter to the chart function
+    new Chartist.Line('.ct-chart', data, options);
     return true;
 });
 
